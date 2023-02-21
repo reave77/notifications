6e1380e155f396c531e7df2c79f6e369cbb344f2
José Carlos Muñoz <jose.munoz@amaris.com>
2023-02-21 19:24:43 +0100

    poc

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,38 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useReducer, useRef, useState } from 'react'
 import './App.css'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import Typography from '@mui/material/Typography'
 
+function getDateTimestamp(date) {
+  return date.startsWith("#")
+    ? Number(date.substring(1)) * 1000
+    : (new Date(date)).getTime()
+}
+
 function App() {
 
   const [notifications, setNotifications] = useState(null)
+  const isFetching = useRef(false);
 
   useEffect(() => {
-    if (notifications === null) fetch("http://localhost/notifications")
-      .then(response => response.json())
-      .then(body => setNotifications(body))
+    if (notifications === null && !isFetching.current) {
+      isFetching.current = true;
+      fetch("https://retoolapi.dev/02UE10/notifications")
+        .then(response => response.json())
+        .then(body => {
+          const freshNotifications = body.filter(notification =>
+            (Date.now() - getDateTimestamp(notification.date) < (1000 * 60 * 60 * 24 * 2)))
+          setNotifications(freshNotifications.sort((a, b) => getDateTimestamp(b.date) - getDateTimestamp(a.date)))
+          const oldNotifications = body.filter(n => !freshNotifications.includes(n))
+          if (oldNotifications.length && freshNotifications.length) {
+            const notificationToDelete = oldNotifications.pop();
+            void fetch(`https://retoolapi.dev/02UE10/notifications/${notificationToDelete.id}`, {
+              method: "DELETE"
+            })
+          }
+        }).finally(() => { isFetching.current = false })
+    }
   }, [notifications])
 
   return (
@@ -20,10 +41,10 @@ function App() {
         (notifications ?? []).map((notification, index) => <Card variant="outlined" key={index} sx={{ m: 3 }}>
           <CardContent>
             <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-              {new Date(notification.date).toLocaleDateString()} {new Date(notification.date).toLocaleTimeString()}
+              {new Date(getDateTimestamp(notification.date)).toLocaleDateString()} {new Date(getDateTimestamp(notification.date)).toLocaleTimeString()}
             </Typography>
             <Typography variant="h5" component="div">
-              {notification.message}
+              {notification.notification}
             </Typography>
           </CardContent>
         </Card>)
